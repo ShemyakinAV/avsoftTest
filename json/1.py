f5426e07fa4788468b9534dafa6e9cfa464aa5e2
ShemyakinAV <antonshemyakin97@gmail.com>
2018-03-19 21:32:17 +0300

    test json0.1
    Добавлен признак ООП - наследование (класс Changedir)
    Добавлена обработка ошибки, которая возникает в случае, если логин или пароль для FTP-сервера неверный.

--- json/1.py
+++ json/1.py
@@ -5,21 +5,40 @@ import json, shutil, ftplib
 filename = "config.json"
 files = open(filename, mode = "w")
 
+class File:
+    name = ""
+    startdir = "" 
+    def setparams(self, name, startdir):
+        self.name = name
+        self.startdir = startdir
+
+class Changedir (File):
+    ftpdir = ""
+    def setdir(self, ftpdir):
+        self.ftpdir = ftpdir
+
+file1 = Changedir()
+file1.setdir("ftp.avsoft.com\avsoft\folder1\ ")
+file1.setparams("file1.txt", "F:\json")
+file2 = Changedir()
+file2.setdir("ftp.avsoft.com\avsoft\folder2\ ")
+file2.setparams("file2.txt", "F:\json")
+
 file1 = {
-    "name": "file1.txt",
-    "startdir" : 'F:\json',
+    "name": str(file1.name),
+    "startdir" : str(file1.startdir),
     "ftpaddress": "ftp.avsoft.com",
     "ftplogin": "avsoft",
     "ftppass": "avsoft",
-    "ftpdir": "ftp.avsoft.com\avsoft\ "
+    "ftpdir": str(file1.ftpdir),
     }
 file2 = {
-    "name": "file2.txt",
-    "startdir" : 'F:\json',
+    "name": str(file2.name),
+    "startdir" : str(file2.startdir),
     "ftpaddress": "ftp.avsoft.com",
     "ftplogin": "avsoft",
     "ftppass": "avsoft",
-    "ftpdir": "ftp.avsoft.com\avsoft\ ",
+    "ftpdir": str(file2.ftpdir),
     }
 
 filelist = []
@@ -37,9 +56,15 @@ ftp = FTP(host = str([file1['ftpaddress']]))
 ftp.login(user='avsoft', passwd = 'avsoft')
 ftp.cwd(str([file1[ftpdir]]))
 
+try:
+    ftp.login(user != 'avsoft', passwd != 'avsoft')
+except ValueError:
+    print("Неверный логин и(или) пароль")
+    
 def place():
     filename1 = "file1.txt"
     filename2 = "file2.txt"
     ftp.storbinary("STOR " + filename1, open(filename1, 'rb'))
     ftp.storbinary("STOR " + filename2, open(filename2, 'rb'))
     ftp.quit()
+
